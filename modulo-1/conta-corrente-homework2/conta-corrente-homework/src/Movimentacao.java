e37aebd47c468f8a833a858a10bb82e8043536fc
Lucas Pereira <82780226+PereirarLucas@users.noreply.github.com>
2022-10-04 23:42:51 -0300

    Delete modulo-1/conta-corrente-homework2 directory

--- modulo-1/conta-corrente-homework2/conta-corrente-homework/src/Movimentacao.java
+++
@@ -1,9 +0,0 @@
-public interface Movimentacao {
-
-        boolean sacar(double valor);
-
-        void depositar(double valor);
-
-        void transferir(ContaCorrente contaCorrente, double valor);
-
-}
